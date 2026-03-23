181e0a80b1999c944468bb02059901cd6c038e18
dev-slife <169207288+dev-slife@users.noreply.github.com>
2026-03-23 00:21:48 -0400

    Initial commit

---
+++ docs/Acknowledgments.md
@@ -0,0 +1,14 @@
+# Acknowledgments
+There are multiple people who worked hard and dependencies used to make VAL possible.
+
+## VAL's Team
+- [dev.slife](https://github.com/dev-slife)
+    - Team Lead 💎
+    - DevOps Lead ⚙️
+    - Backend & Frontend Developer 🐛
+
+## Dependencies
+- VAST 📏
+- [Docker](https://www.docker.com/) 🐳
+- [Node.js](https://nodejs.org/en) ⚡
+- [MongoDB](https://www.mongodb.com/) 🍃
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import * as vast from "./libs/vast/algebra.mjs";
+
+const bodyParser = require('body-parser');
+
+const path = require('path');
+const express = require('express');
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use(bodyParser.json());
+
+// API routes AFTER static
+app.get('/api/test', (req, res) => res.json({ status: 'API works' }));
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    const ans = vast.simplify("2x+3x-5");
+    console.log("VAST Check: ", ans);
+});
