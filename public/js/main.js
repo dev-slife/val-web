67e4e1c7c64f6826727feb0c2be5070141bf5118
dev-slife <169207288+dev-slife@users.noreply.github.com>
2026-03-29 20:04:31 -0400

    Added routing and all main web app pages.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,50 @@
+/**
+ * Author: dev.slife
+ * Date Created: 3/23/26
+ * Date Updated: 3/29/26
+ * Description:
+ *      Handles main frontend interaction.
+ */
+
+
+// --------------------------- HELPER FUNCTIONS --------------------------- //
+
+async function expressPage(url) {
+    try {
+        const response = await fetch(url);
+        window.location.href = response.url;
+        return response.url;
+    } catch (error) {
+        console.error("Could not fetch Express page using: ", url);
+        return null;
+    }
+}
+
+async function expressFetch(url) {
+    try {
+        const response = await fetch(url);
+        console.log(response);
+        // const data = await response.json();
+        // console.log(data);
+        return "";
+    } catch (error) {
+        console.error("Could not fetch Express API using: ", url);
+        return null;
+    }
+}
+
+
+// --------------------------- MODULE FUNCTIONS --------------------------- //
+
 export function changePage(page="home", is_index_file=false) {
-    const base_url = (is_index_file) ? "./pages/": "../pages/";
-    const file_name = (page.includes(".html")) ? page: page+".html";
-    const url = base_url + file_name;
-    window.location.href = url;
+    // First try to use the express route
+    const expressURL = window.location.href + page;
+    if (!expressPage(expressURL)) {
+        // If not default to html file
+        const base_url = (is_index_file) ? "./pages/": "../pages/";
+        const file_name = (page.includes(".html")) ? page: page+".html";
+        const url = base_url + file_name;
+        window.location.href = url;
+    }
+
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,64 @@
+/**
+ * Author: dev.slife
+ * Date Created: 3/23/26
+ * Date Updated: 3/29/26
+ * Description:
+ *      Handles all main API communication between the frontend, backend, and web app services.
+ */
+
+
+
+// --------------------------- IMPORTS & CONSTANTS --------------------------- //
+
 import * as vast from "./libs/vast/api/algebra.mjs";
-
 import bodyParser from "body-parser";
-
 import { fileURLToPath } from 'url';
 import path from "path";
 import express from "express";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const __root = path.dirname(__dirname);
+const __public = path.join(__root, 'public');
 const app = express();
 
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// --------------------------- INITIALIZATION --------------------------- //
+
+app.use(express.static(__public, {
+    extensions: ['html']
+}));
 
 app.use(bodyParser.json());
 
-// API routes AFTER static
-app.get('/api/test', (req, res) => res.json({ status: 'API works' }));
 
+
+// --------------------------- ROUTING --------------------------- //
+
+app.get('/home', (_, res) => {
+    res.sendFile(path.join(__public, 'pages', 'home.html'));
+});
+
+app.get('/about', (_, res) => {
+    res.sendFile(path.join(__public, 'pages', 'about.html'));
+});
+
+app.get('/terms', (_, res) => {
+    res.sendFile(path.join(__public, 'pages', 'terms.html'));
+});
+
+app.get('/privacy', (_, res) => {
+    res.sendFile(path.join(__public, 'pages', 'privacy.html'));
+});
+
+app.get('/copyright', (_, res) => {
+    res.sendFile(path.join(__public, 'pages', 'copyright.html'));
+});
+
+
+
+// --------------------------- RUNNING THE APP --------------------------- //
 
 const PORT = process.env.PORT || 3000;
 
